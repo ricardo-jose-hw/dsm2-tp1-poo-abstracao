70a891b62d1079f3ddd56343fb63312e6a00ab62
Ricardos Jose <rcrdo.dev@gmail.com>
2026-09-24 11:43:27 -0300

    Início do repositório com classe Carro preenchida.

---
+++ src/exercicio01/Carro.java
@@ -0,0 +1,100 @@
+package exercicio01;
+
+public class Carro {
+	public Carro(String mc,String md, String plc) {
+
+		this.marca = mc;
+
+		this.modelo = md;
+
+		this.placa = plc;
+
+		this.ligar = false;
+		
+		this.capacidadeTanque = 0;
+
+	}
+	
+	private String marca;
+	public String getMarca() {
+		return this.marca;
+	}
+	
+	private String modelo;
+	public String getModelo() {
+		return this.modelo;
+	}
+
+	private boolean ligar;
+
+	private String placa;
+
+	public String getPlaca() {
+		return this.placa;
+	}
+	
+	private double capacidadeTanque;
+
+	public double getTanque() {
+		return this.capacidadeTanque;
+	}
+	
+	public void acelerar(int velocidade) {
+		System.out.println("ACELEROU " + velocidade);
+
+	}
+
+	
+	public double abastecer(double combustivel) {
+		this.capacidadeTanque += combustivel;
+		return capacidadeTanque;
+		
+	}
+
+	
+
+	public void ligar() {
+
+	if(ligar == false) {
+
+		System.out.println("TENTANDO LIGAR: LIGANDO CARRO");
+
+		ligar = true;
+
+	}
+
+	else {
+
+		System.out.println("TENTANDO LIGAR: NÃO É POSSIVEL. DESLIGUE PRIMEIRO");
+
+	}
+
+	}
+
+	public void desligar() {
+
+		if(ligar == true) {
+
+			System.out.println("TENTANDO DESLIGAR: DESLIGANDO..");
+
+			ligar = false;
+
+		}
+
+		else {
+
+			System.out.println("TENTANDO DESLIGAR: LIGUE PRIMEIRO PARA DESLIGAR");
+
+		}
+		
+
+	}
+
+	public void buzinar() {
+
+		System.out.println("BEEEEEEEEEEEEEEEEEEEEEEEE");
+
+	}
+}
+
+
